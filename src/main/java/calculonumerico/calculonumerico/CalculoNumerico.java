805abd114850e69be5a45ea36dc7a23dc18c9765
Andres Rangel <andres.rangel@correo.unimet.edu.ve>
2025-03-05 15:14:43 -0400

    primer commit

---
+++ src/main/java/calculonumerico/calculonumerico/CalculoNumerico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculonumerico.calculonumerico;
+
+/**
+ *
+ * @author DELL
+ */
+public class CalculoNumerico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
